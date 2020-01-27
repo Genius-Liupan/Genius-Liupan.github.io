fc2ef4090f79fb0b7d5458d457661cc3eb3a8979
Genius-Liupan <60336101+Genius-Liupan@users.noreply.github.com>
2020-01-27 13:55:15 +0800

    Initial commit

---
+++ precache-manifest.f91959b64f5ebcff635f9773cb617205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6efba18933639c76c1d19b029792907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1eba65c0664e78e4745",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eb7927e968e4e8eeda6d",
+    "url": "/static/js/2.194cc245.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.194cc245.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1eba65c0664e78e4745",
+    "url": "/static/js/main.6985bcf3.chunk.js"
+  },
+  {
+    "revision": "c22b7a7b1f273bbdbadd",
+    "url": "/static/js/runtime-main.52d190d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
